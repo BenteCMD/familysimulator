e9d4d0a6c5e9dd9c8a722df61f120dd6eb084394
BenteCMD <112807135+BenteCMD@users.noreply.github.com>
2023-01-03 13:09:00 +0100

    Update scriptindex.js

--- js/scriptindex.js
+++ js/scriptindex.js
@@ -1,1 +1,8 @@
+var startButton = document.querySelector("#startbutton");
+var startgame = new Audio("sounds/ding-dong.mp3");
 
+function dingdong () {
+    startgame.play("sounds/ding-dong.mp3");
+}
+
+startButton.addEventListener("mouseover", dingdong);
